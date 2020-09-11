df9995751b0508fa132c6620387925f3ea0bb78a
txyyf233 <1515721820@qq.com>
2020-09-11 16:22:58 +0800

    vue-bokiboki窗口大小监听

--- vue-bokiboki/src/App.vue
+++ vue-bokiboki/src/App.vue
@@ -1,16 +1,37 @@
 <template>
-  <div id="app">
+  <div id="app" :style="{width:appWidth + 'px'}">
     <router-view/>
   </div>
 </template>
 
 <script>
+var oldWidth = document.documentElement.clientWidth
+oldWidth = oldWidth > 600 ? oldWidth : 600
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      appWidth: oldWidth
+    }
+  },
+  mounted () {
+    var newWidth = document.documentElement.clientWidth
+    window.onresize = () => {
+      if (newWidth > 600) {
+        oldWidth = newWidth
+        this.appWidth = newWidth
+      }
+    }
+  }
 }
 </script>
 
 <style>
   #app {
+    height: 800px;
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    background-color: rgba(0,0,0,0.2);
   }
 </style>
