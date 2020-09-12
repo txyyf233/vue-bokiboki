cf5dff2380c58639543bd4c2202ea93538cf3d15
txyyf233 <1515721820@qq.com>
2020-09-13 00:48:54 +0800

    app-head

--- vue-bokiboki/src/App.vue
+++ vue-bokiboki/src/App.vue
@@ -1,5 +1,37 @@
 <template>
-  <div id="app" :style="{width : appWidth + 'px'}">
+  <div id="app">
+    <div id="vu-main" :style="{width : mainWidth + 'px',backgroundColor: colorTheme}">
+      <ul id="vu-head">
+        <li id="vu-img-li">
+          <a><img id="vu-img" src="@/assets/bokiboki.png"></a>
+        </li>
+        <li id="vu-search-li">
+          <el-input id="vu-search" placeholder="请输入搜索内容" v-model="vuSearch">
+            <i id="vu-search-icon" @click="searchMethods" class="el-icon-search el-input__icon" slot="suffix"></i>
+          </el-input>
+        </li>
+        <li id="vu-menu-li">
+          <el-dropdown>
+              <span class="el-dropdown-link">
+                <i class="el-icon-caret-bottom el-icon--right"
+                   style="position: relative;left: -15px"></i>
+              </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>个人中心</el-dropdown-item>
+              <el-dropdown-item>采集板</el-dropdown-item>
+              <el-dropdown-item>设置</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          <div>
+            <el-avatar :size="40" :src="headSrc" @error="errorHandler" fit="scale-down">
+              <img src="@/assets/errorImg.png"/>
+            </el-avatar>
+          </div>
+          <div><i class="el-icon-message-solid"></i></div>
+          <div><i class="el-icon-menu"></i></div>
+        </li>
+      </ul>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -7,20 +39,36 @@
 <script>
 var oldWidth = window.innerWidth
 var newWidth
-oldWidth = oldWidth > 600 ? oldWidth : 600
+var minWindow = 720
+oldWidth = oldWidth > minWindow ? oldWidth : minWindow
 export default {
   name: 'App',
   data () {
     return {
-      appWidth: oldWidth
+      colorTheme: '#438a5e',
+      mainWidth: oldWidth,
+      vuSearch: '',
+      result: '',
+      headSrc: 'http://hbimg.huabanimg.com/8603d73dfe2117e717f21dfcdeffd06a5bbd3a4a225ed3-9OdaSK_fw658/format/webp',
+    }
+  },
+  methods: {
+    searchMethods: function () {
+      if(this.vuSearch){
+        this.result = this.vuSearch
+      }
+    },
+    errorHandler: function () {
+      return true
     }
   },
   mounted () {
     window.onresize = () => {
       newWidth = window.innerWidth
-      if (newWidth > 600) {
+      console.log('当前浏览器窗口宽度' + newWidth + 'px')
+      if (newWidth > minWindow) {
         oldWidth = newWidth
-        this.appWidth = oldWidth
+        this.mainWidth = oldWidth
       }
     }
   }
@@ -29,10 +77,74 @@ export default {
 
 <style>
   #app {
-    height: 1000px;
-    position: fixed;
+    position: absolute;
     top: 0px;
     left: 0px;
     background-color: rgba(0,0,0,0.2);
+  }
+  #vu-main {
+    width: 100%;
+    height: 60px;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    margin: 0px 0px;
+    padding: 0px 0px;
+    z-index: 999;
+  }
+  #vu-head {
+    width: 100%;
+    height: 60px;
+    margin: 0px 0px;
+    padding: 0px 0px;
+  }
+  li {
+    list-style: none;
+  }
+  #vu-img-li {
+    width: 20%;
+    height: 100%;
+    left: 0px;
+    float: left;
+  }
+  #vu-img {
+    margin: 10px 0px;
+    height: 40px;
+    margin-left: 10%;
+  }
+  #vu-search-li {
+    width: 40%;
+    height: 100%;
+    float: left;
+  }
+  #vu-search {
+    height: 40px;
+    margin: 10px 0px;
+    border-radius: 30px;
+    font-size: 15px;
+  }
+  #vu-search-icon {
+    height: 40px;
+    margin: 10px 0px;
+    border-radius: 30px;
+    font-size: 20px;
+    margin-right: 15px;
+  }
+  #vu-menu-li {
+    width: 40%;
+    height: 100%;
+    float: left;
+  }
+  #vu-menu-li div {
+    height: 40px;
+    float: right;
+    padding:0px 20px;
+    margin:10px 0px;
+
+  }
+  #vu-menu-li i {
+    line-height:40px;
+    color: rgba(0,0,0,0.5);
+    font-size: 25px;
   }
 </style>
--- vue-bokiboki/src/components/Main.vue
+++ vue-bokiboki/src/components/Main.vue
@@ -1,20 +1,5 @@
 <template>
   <div>
-    <div id="vuMain">
-      <a>
-        <img id="vuHeadImg" src="@/assets/bokiboki.png">
-      </a>
-      <ul id="vuHeadUl" style="background-color: lightgray">
-        <li style="background-color: aqua">
-          <div id="vuSearch" style="border-radius: 30px;background-color: lightcoral">
-            <input id="vuSearch-input" placeholder="请输入内容" style="border: 0px;background-color: rgba(0,0,0,0)">
-            <a id="vuSearch-search"><i class="el-icon-search"></i></a>
-          </div>
-        </li>
-        <li>
-        </li>
-      </ul>
-    </div>
   </div>
 </template>
 
@@ -23,7 +8,6 @@ export default {
   name: 'Main',
   data () {
     return {
-      vuInput: ''
     }
   }
 }
@@ -31,25 +15,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  #vuMain {
-    width: 100%;
-    height: 60px;
-    position: fixed;
-    top: 0px;
-    z-index: 999;
-    background-color: #438a5e;
-  }
-  #vuHeadImg {
-    padding: 10px 3%;
-    height: 60px;
-  }
-  #vuHeadUl {
-    height: 60px;
-    float: right;
-    margin-top: 0px;
-  }
-  #vuHeadUl li{
-    list-style: none;
-    height: 100%;
-  }
 </style>
