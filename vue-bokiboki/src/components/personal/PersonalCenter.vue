71379ff8e405265c6c78f1a9396a75bc820d5c00
txyyf233 <1515721820@qq.com>
2020-12-24 14:03:40 +0800

    123

---
+++ vue-bokiboki/src/components/personal/PersonalCenter.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <!---------------------------------------导航栏----------------------------------->
+    <headTop></headTop>
+    <!---------------------------------------个人主页----------------------------------->
+    <el-row style="margin-top: 5px">
+      <el-col class="hidden-md-and-down" :lg="4">&nbsp;</el-col>
+      <el-col  :xs="24" :sm="24"  :lg="16">
+        <div style="background-color: white;margin: 70px 0 20px;width: 130px;text-align: center;padding-top: 5px">
+          <img :src="headSrc" style="width: 120px"/>
+        </div>
+        <el-collapse accordion v-model="activeNames">
+          <el-collapse-item name="发布">
+            <template slot="title">
+              <span style="padding-left: 20px;font-size: 20px;color: rgba(0,0,0,0.6)">发布</span>
+            </template>
+            <div >
+            </div>
+          </el-collapse-item>
+          <el-collapse-item  name="消息">
+            <template slot="title">
+              <span style="padding-left: 20px;font-size: 20px;color: rgba(0,0,0,0.6)">消息</span>
+            </template>
+            <div></div>
+          </el-collapse-item>
+          <el-collapse-item  name="采集板">
+            <template slot="title">
+              <span style="padding-left: 20px;font-size: 20px;color: rgba(0,0,0,0.6)">采集板</span>
+            </template>
+            <div></div>
+          </el-collapse-item>
+          <el-collapse-item  name="个人资料">
+            <template slot="title">
+              <span style="padding-left: 20px;font-size: 20px;color: rgba(0,0,0,0.6)">个人资料</span>
+            </template>
+            <div></div>
+          </el-collapse-item>
+        </el-collapse>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+import HeadTop from '@/components/middle/HeadTop'
+export default {
+  name: 'PersonalCenter',
+  components: {HeadTop},
+  data () {
+    return {
+      activeNames: '发布',
+      headSrc: 'https://img.erp.ctrl.com.cn/group1/M00/01/9C/PNAg4F_JnxiAb8nIAAE5fHZhYlo865.jpg'
+    }
+  }
+}
+</script>
+<style scoped>
+  /deep/ .el-collapse-item__header{
+    background-color: rgba(102,191,191,0.5);
+    height: 70px;
+  }
+</style>
